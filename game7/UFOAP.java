704d9759e39df4623fd35023074c8103f173cfc8
yu01080 <19114@hakodate.kosen-ac.jp>
2023-11-02 10:05:18 +0900

    UFOAP追加

---
+++ game7/UFOAP.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class UFOAP here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UFOAP extends Actor
+{
+    /**
+     * Act - do whatever the UFOAP wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}

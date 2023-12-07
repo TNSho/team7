df06e7a4162ce92dc6291117b20788362eda85ed
NakanoMugen <19137@hakodate.kosen-ac.jp>
2023-12-07 09:53:18 +0900

    TNsho救済

--- game7/UFOAP.java
+++ game7/UFOAP.java
@@ -8,12 +8,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class UFOAP extends Actor
 {
-    /**
-     * Act - do whatever the UFOAP wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int jeda = 0;
+    public int kuat = 30;
+    private boolean bisaTembak = true;
+    public void gerak()
+    {
+        setLocation(getX(), getY() + 1);
+        if (getX() > 496)
+        {
+            setLocation(Greenfoot.getRandomNumber(900), Greenfoot.getRandomNumber(75));
+        }
+    }
     public void act() 
     {
+        gerak();
+        if (kuat == 0)
+        {
+            kuat = 30;
+        }
+        if (jeda > 0)
+            jeda--;
+        if (jeda == 1 && bisaTembak)
+            getWorld().addObject(new UFOAP(), getX()+50, getY());
+        if (jeda == 0)
+            jeda = 115;
 
     }    
 }
